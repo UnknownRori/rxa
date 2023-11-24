f71b75e5bac742e7ea56afc453610134201a1a39
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-11-25 05:55:22 +0700

    📦 Implement print grid

--- src/print.rs
+++ src/print.rs
@@ -1,9 +1,41 @@
+use console::Style;
+use term_grid::{self, Cell, Direction, Filling, Grid, GridOptions};
+use terminal_size;
+
 use crate::fs::file::{RItemDisplay, RxaFile};
 
 pub type DisplayItems = Vec<RxaFile>;
 
 pub fn print_grid(items: DisplayItems) {
-    unimplemented!()
+    // TODO : Make proper grid, (Maybe custom implementation)
+    let (terminal_size::Width(width), terminal_size::Height(height)) =
+        terminal_size::terminal_size().expect(
+            format!(
+                "{}",
+                Style::new()
+                    .red()
+                    .apply_to("[!] Cannot get width and height of your current terminal")
+            )
+            .as_str(),
+        );
+
+    let mut grid = Grid::new(GridOptions {
+        filling: Filling::Spaces(4),
+        direction: Direction::LeftToRight,
+    });
+
+    for item in items {
+        let display = match item {
+            RxaFile::File(file) => file.display(),
+            RxaFile::Directory(dir) => dir.display(),
+        };
+
+        let cell = Cell::from(display);
+        grid.add(cell);
+    }
+
+    println!("{}:{}", width, height);
+    println!("{}", grid.fit_into_width(width.into()).unwrap());
 }
 
 pub fn print_oneline(items: DisplayItems) {
